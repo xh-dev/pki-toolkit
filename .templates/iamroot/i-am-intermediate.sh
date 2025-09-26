f12684ca62ae3e04661be7d3bd282a30077b9758
xeth.hung@HTGVMS250821-1 <adamhung12@gmail.com>
2025-09-26 07:10:11 +0000

    update

--- .templates/iamroot/i-am-intermediate.sh
+++ .templates/iamroot/i-am-intermediate.sh
@@ -22,7 +22,7 @@ cp -fr .templates/common .
 ./common/create-key.sh ca.key.pem
 
 cp .templates/iamroot/gen-csr.sh .
-cp .templates/iamroot/sign-*-cert.pem .
+cp .templates/iamroot/sign-*-cert.sh .
 cp .templates/iamroot/get-intermediate-config.sh common/get-config.sh
 ./common/get-config.sh
 
