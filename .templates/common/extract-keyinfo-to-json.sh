5f7e5da62fca401153d11e5533af5139bee2c915
xeth.hung@HTGVMS250821-1 <adamhung12@gmail.com>
2025-09-26 03:28:05 +0000

    update

--- .templates/common/extract-keyinfo-to-json.sh
+++ .templates/common/extract-keyinfo-to-json.sh
@@ -1,8 +1,37 @@
 cert_file=$1
 
 cert_data="$(openssl x509 -noout -text -in $cert_file|grep Subject:)"
+#echo $cert_data
 function get_value(){
 	v=$1
 	echo $cert_data | grep -Po "$v"
 }
-echo "{\"C\":\"$(get_value '(?<=C = )(.*)(?=, ST)')\",\"ST\":\"$(get_value '(?<=ST = )(.*)(?=, L)')\", \"L\":\"$(get_value '(?<=L = )(.*)(?=, O )')\",\"O\":\"$(get_value '(?<=O = )(.*)(?=, OU)')\",\"OU\":\"$(get_value '(?<=OU = )(.*)(?=, CN)')\",\"CN\":\"$(get_value '(?<=CN = )(.*)(?=, emailAddress)')\",\"email\":\"$(get_value '(?<=emailAddress = )(.*)$')\" }" | jq
+
+function replacing(){
+	v=$1
+	echo "$v" | sed -e "s|^[\"]\(.*\)$|\1|g" | sed -e "s|^\(.*\)[\"]$|\1|" | sed -e "s|\"|\\\\\"|g"
+}
+
+C="$(get_value '(?<=C = )(.*)(?=, ST)')" 
+C="$(replacing "$C")" 
+#echo $C
+ST="$(get_value '(?<=ST = )(.*)(?=, L)')"
+ST="$(replacing "$ST")" 
+#echo $ST
+L="$(get_value '(?<=L = )(.*)(?=, O )')"
+L="$(replacing "$L")" 
+#echo $L
+O="$(get_value '(?<=O = )(.*)(?=, OU)')"
+O="$(replacing "$O")" 
+#echo $O
+OU="$(get_value '(?<=OU = )(.*)(?=, CN)')"
+OU="$(replacing "$OU")" 
+#echo $OU
+CN="$(get_value '(?<=CN = )(.*)(?=, emailAddress)')"
+CN="$(replacing "$CN")" 
+#echo $CN
+email="$(get_value '(?<=emailAddress = )(.*)$')"
+email="$(replacing "$email")" 
+#echo $email
+
+echo "{\"C\":\"$C\",\"ST\":\"$ST\", \"L\":\"$L\",\"O\":\"$O\",\"OU\":\"$OU\",\"CN\":\"$CN\",\"email\":\"$email\"}" | jq
